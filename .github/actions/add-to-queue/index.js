0aacb7dc7a5d31bad01287ec819b9a6ca9837164
Dominik Buszowiecki <DominikB2014@gmail.com>
2021-05-19 19:37:03 -0400

    Create custom status check

---
+++ .github/actions/add-to-queue/index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const [
+      gitHubRepoOwner,
+      gitHubRepoName,
+    ] = process.env.GITHUB_REPOSITORY.split('/');
+    const gitHubSha = process.env.GITHUB_SHA;
+    const gitHubToken = process.env.TOKEN;
+
+    const octokit = new github.GitHub(gitHubToken);
+
+    octokit.checks.create({
+      owner: gitHubRepoOwner,
+      repo: gitHubRepoName,
+      name: 'Check Created by API',
+      head_sha: gitHubSha,
+      status: 'completed',
+      conclusion: 'success',
+      output: {
+        title: 'Check Created by API',
+        summary: `# All good ![step 1](https://commons.wikimedia.org/wiki/File:Flat_tick_icon.svg "Step 1")`,
+      },
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
