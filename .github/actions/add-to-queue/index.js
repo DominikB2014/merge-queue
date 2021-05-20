82d90375ae9a585e16a5a4066fd0bbd9cd8b0031
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2021-05-19 23:34:51 -0400

    test pr workflow (#1)
    * test pr workflow
    
    * Create custom status check
    
    * Update pr.yml
    
    * test
    
    * update
    
    * update
    
    * update
    
    * Update
    
    * update
    
    * Log sha
    
    * await
    
    * test
    
    * LOG
    
    * Post to sqs
    
    * test sqs
    
    * Done
    
    * add message group id
    
    * test

---
+++ .github/actions/add-to-queue/index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const AWS = require('aws-sdk');
+const PRID = process.env.PRID;
+
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+async function run() {
+  try {
+    const [
+      gitHubRepoOwner,
+      gitHubRepoName,
+    ] = process.env.GITHUB_REPOSITORY.split('/');
+    const gitHubSha = process.env.GITHUB_SHA;
+    const gitHubToken = process.env.TOKEN;
+
+    console.log(gitHubSha);
+
+    const octokit = github.getOctokit(gitHubToken);
+
+    // const affectedApps = execSync('yarn nx affected:apps');
+    const affectedApps = ['my-app1', 'my-app2'];
+
+    const status = await sqs
+      .sendMessage({
+        QueueUrl:
+          'https://sqs.us-east-1.amazonaws.com/425145600464/merge-queue.fifo',
+        MessageBody: JSON.stringify({ pr: PRID, affectedApps }),
+        MessageGroupId: 'queue',
+        MessageDeduplicationId: PRID,
+      })
+      .promise();
+    console.log(status);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
